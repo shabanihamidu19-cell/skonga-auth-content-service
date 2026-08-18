264533f4680fc5a536307b92631bbdc662672deb
KidCoder Tz <shabanihamidu19@gmail.com>
2026-08-18 22:16:43 +0300

    feat: content controller

---
+++ src/controllers/contentController.js
@@ -0,0 +1,58 @@
+'use strict';
+const contentService = require('../services/contentService');
+const { publicKey } = require('../services/fileStorage');
+
+async function create(req, res, next) {
+  try {
+    const { type, title, body, metadata } = req.body || {};
+    let storageKey = null;
+    let mimeType = null;
+    let size = null;
+    if (req.file) {
+      storageKey = publicKey(req.file.filename);
+      mimeType = req.file.mimetype;
+      size = req.file.size;
+    }
+    const item = contentService.createContent(req.user.id, {
+      type: type || (req.file ? 'image' : 'chat'),
+      title,
+      body,
+      metadata,
+      storageKey,
+      mimeType,
+      size,
+    });
+    res.status(201).json({ content: item });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function list(req, res, next) {
+  try {
+    const items = contentService.listContent(req.user.id, req.query);
+    res.json({ items });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getOne(req, res, next) {
+  try {
+    const item = contentService.getContent(req.user.id, req.params.id);
+    res.json({ content: item });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function remove(req, res, next) {
+  try {
+    const result = contentService.deleteContent(req.user.id, req.params.id);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { create, list, getOne, remove };
