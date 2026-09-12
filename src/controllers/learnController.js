2482cf7d4981f50e956dab98758b04c4bd01d4cc
KidCoder Tz <shabanihamidu19@gmail.com>
2026-09-12 10:12:56 +0300

    feat(learn): catalog endpoint controller

--- src/controllers/learnController.js
+++ src/controllers/learnController.js
@@ -14,7 +14,11 @@ async function getProgress(req, res, next) {
   try {
     const subject = req.query.subject;
     const items = await learnService.getProgress(req.user.id, { subject });
-    res.json({ items });
+    res.json({
+      ok: true,
+      items,
+      algorithm: { version: 2, masteredThreshold: 2, reviewAfterDays: 7 },
+    });
   } catch (err) {
     next(err);
   }
@@ -26,10 +30,22 @@ async function getNext(req, res, next) {
       subject: req.query.subject,
       form: req.query.form,
     });
+    res.json({ ok: true, ...data });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getCatalog(req, res, next) {
+  try {
+    const data = learnService.getCatalog({
+      subject: req.query.subject,
+      form: req.query.form,
+    });
     res.json(data);
   } catch (err) {
     next(err);
   }
 }
 
-module.exports = { postResult, getProgress, getNext };
+module.exports = { postResult, getProgress, getNext, getCatalog };
